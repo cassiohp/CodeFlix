960379a192b213a6513abdf3815eb8bcbd53cc35
Cássio H <cassio375@gmail.com>
2022-04-25 03:00:39 -0300

    Adicionado Carrossel e Miniaturas

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        800:{
+            items:5
+        }
+    }
+})
